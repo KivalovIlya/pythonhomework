1b5d80052a043a868bbc2c24523b5a0f7c151139
Ilya Kivalov <kivalovilya@icloud.com>
2021-11-19 23:10:11 +0300

    Создание папки lesson1

--- lesson1/task5.py
+++ lesson1/task5.py
@@ -2,7 +2,7 @@ company_income = int(input("Введите прибыль компании - "))
 company_loss = int(input("Введите убыль компании - "))
 if company_income > company_loss:
     print("Компания работает с прибылью")
-    print(f"{int((company_income/company_loss) * 100)}% - рентабельность") # Формула рентабельности: (доход/расход)*100%
+    print(f"{company_income/(company_income-company_loss)} - рентабельность") # Формула рентабельности: (доход/расход)*100%
     employee = int(input("Введите кол-во сотрудников - "))
     print(f"{company_income/employee} - прибыль на одного сотрудника")
 else:
